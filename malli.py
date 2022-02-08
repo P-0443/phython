5b645128cda8ecdd6e5ad3bb6558f14bfc5ace2e
P-0443 <98080157+P-0443@users.noreply.github.com>
2022-02-08 09:13:46 +0530

    Add files via upload

---
+++ malli.py
@@ -0,0 +1,18 @@
+print('mallikarjuna')
+
+# #variables
+# counters = 100
+# miles =  1000.0
+# name =  malli
+# print (count)
+# print (mile)
+# print (name)
+# #string
+# str = 'mallikarjuna'
+count=10
+mile=10000.000
+str="mallikarjuna"
+print("count : ",count,end=" ")
+print("mile : ",mile,end=" ")
+print("str : ",str)
+
---
+++ swap.py
@@ -0,0 +1,14 @@
+
+a = 11
+b = 7
+a, b = b, a
+print(a)
+print(b)
+x = 5
+y = 6
+print("before swapping:")
+print("value of x:",x," and y: ",y)
+x,y = y,x
+print("after swapping:")
+print("value of x:",x," and y: ",y)
+
